f0abedcab4e34f30e38a7121a1471e14f4892d8a
andrea9llo <andrea.novello93@gmail.com>
2019-10-21 22:50:51 +0200

    dadi

---
+++ dadi/js/main.js
@@ -0,0 +1,29 @@
+// Gioco dei dadi, chi fa di più vince
+
+// dichiaro le variabili
+var numeroUtente, numeroComp ;
+
+// creo un alert per iniziare il gioco
+alert("Clicca per lanciare i dadi");
+
+// dichiaro la funzione per generare un numero random da 1 a 6
+numeroUtente = getRndInteger(1,6);
+numeroComp = getRndInteger(1,6);
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+// console.log(numeroComp,numeroUtente);
+
+// dichiaro la condizione per sapere chi vince
+
+if (numeroUtente > numeroComp) {
+  // console.log("tuo numero " + numeroUtente + " numero computer " + numeroComp + " hai vinto");
+  document.getElementById('game-dadi').innerHTML = "Il tuo numero è: " + numeroUtente + "...il numero del computer è: " + numeroComp + "...HAI VINTO";
+} else if (numeroUtente < numeroComp) {
+    // console.log("tuo numero " + numeroUtente + " numero computer " + numeroComp + " hai perso");
+    document.getElementById('game-dadi').innerHTML = "Il tuo numero è: " + numeroUtente + "...il numero del computer è: " + numeroComp + "...HAI PERSO";
+} else {
+  // console.log("tuo numero " + numeroUtente + " numero computer " + numeroComp + " pareggio");
+  document.getElementById('game-dadi').innerHTML = "Il tuo numero è: " + numeroUtente + "...il numero del computer è: " + numeroComp + "...PAREGGIO";
+}
