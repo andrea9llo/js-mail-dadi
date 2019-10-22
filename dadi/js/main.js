d0e8059ba3128433a94c72abf88965025ba8020c
andrea9llo <andrea.novello93@gmail.com>
2019-10-22 20:32:28 +0200

    mod con regole viste fin ora

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -7,13 +7,16 @@ var numeroUtente, numeroComp ;
 alert("Clicca per lanciare i dadi");
 
 // dichiaro la funzione per generare un numero random da 1 a 6
-numeroUtente = getRndInteger(1,6);
-numeroComp = getRndInteger(1,6);
-
-function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
+// numeroUtente = getRndInteger(1,6);
+// numeroComp = getRndInteger(1,6);
+//
+// function getRndInteger(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1) ) + min;
+// }
 // console.log(numeroComp,numeroUtente);
+// dichiaro le variabili dei dadi cosi da far uscire  un numero da 1 a 6
+numeroUtente = Math.floor(Math.random() * 6) + 1;
+numeroComp =  Math.floor(Math.random() * 6) + 1;
 
 // dichiaro la condizione per sapere chi vince
 
